349848070ad08f49abbac99402b118dd65a4bc8f
Soma Hansel <somahansel@gmail.com>
2022-11-28 10:08:15 +0100

    copied original swin transformer

--- models/swin_transformer_block.py
+++ models/swin_transformer_block.py
@@ -27,13 +27,19 @@ class Mlp(nn.Module):
 def window_partition(x, window_size):
     """
     Args:
-        x: (B, H, W, C)
+        x: (B, H, W, D, C)
         window_size (int): window size
     Returns:
-        windows: (num_windows*B, window_size, window_size, C)
+        windows: (num_windows*B, window_size, window_size, window_size, C)
     """
-    B, H, W, C = x.shape
-    x = x.view(B, H // window_size, window_size, W // window_size, window_size, C)
+    B, H, W, D, C = x.shape
+    x = x.view(
+        B,
+        H // window_size,
+        window_size,
+        W // window_size,
+        window_size, C
+    )
     windows = x.permute(0, 1, 3, 2, 4, 5).contiguous().view(-1, window_size, window_size, C)
     return windows
 
