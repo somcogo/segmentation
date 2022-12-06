40e591bda51b224da3e21667a687603e60c1a9e8
Soma <51997913+somcogo@users.noreply.github.com>
2022-12-06 16:07:08 +0100

    Model (#3)
    * removed weight init
    
    * copied original swin transformer
    
    * copied original swin transformer
    
    * updated .gitignore
    
    * window partition, reverse, attention
    
    * updated gitignore
    
    * swin transformer block adjusted for 3d
    
    * added models
    
    * model commit
    
    * Delete aux directory
    
    * updated gitignore
    
    * unet change
    
    * commit before pull
    
    * model working
    
    * import cleanup

---
+++ lightning.py
@@ -0,0 +1,74 @@
+import torch
+from torch import nn
+import pytorch_lightning as pl
+from pytorch_lightning.loggers import TensorBoardLogger
+from models.swin_transformer_block import SwinTransformer
+from models.unetblocks import UNetConvBlock, UNetUpBlock
+from models.swinunetr import SwinUNETR
+from utils.data_loader import getDataLoader
+
+import os
+
+class SwinUNETRModule(pl.LightningModule):
+	def __init__(self,
+		img_size=64,
+		patch_size=8,
+		in_chans=1,
+		num_classes=2,
+		embed_dim=48,
+		window_size=2,
+		depths=[2, 2, 2, 2],
+		num_heads=[3, 6, 12, 24],
+	):
+		super().__init__()
+		self.swinunetr = SwinUNETR(img_size=img_size,
+			patch_size=patch_size,
+			in_chans=in_chans,
+			num_classes=num_classes,
+			embed_dim=embed_dim,
+			window_size=window_size,
+			depths=depths,
+			num_heads=num_heads,
+		)
+
+	def configure_optimizers(self):
+		optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+		return optimizer
+
+	def training_step(self, train_batch, batch_idx):
+		x, y = train_batch
+		features = self.swinunetr(x)
+		out = self.head(features)
+		loss_fn = nn.CrossEntropyLoss(reduction='sum')
+		y = y.long()
+		loss = loss_fn(out, y.squeeze())
+		return loss
+
+	def validation_step(self, val_batch, batch_idx):
+		x, y = val_batch
+		features = self.swinunetr(x)
+		out = self.head(features)
+		loss_fn = nn.CrossEntropyLoss(reduction='sum')
+		y = y.long()
+		loss = loss_fn(out.view(16, 2, -1), y.view(16, -1))
+		self.log('val_loss', loss)
+
+# data
+train_loader = getDataLoader(16)
+val_loader = getDataLoader(16)
+
+# model
+model = SwinUNETRModule(img_size=64, patch_size=2, embed_dim=24, depths=[2, 2, 2], num_heads=[3, 6, 12])
+
+# logger
+logger = TensorBoardLogger(save_dir=os.getcwd(), version=1, name="lightning_logs")
+
+# training
+trainer = pl.Trainer(
+	accelerator='gpu',
+	max_epochs=100,
+	limit_train_batches=0.5,
+	logger=logger
+	)
+trainer.fit(model, train_loader, val_loader)
+    
---
+++ models/swinunetr.py
@@ -0,0 +1,68 @@
+from .swin_transformer_block import SwinTransformer
+from .unetblocks import UNetConvBlock, UNetUpBlock
+import torch.nn as nn
+
+class SwinUNETR(nn.Module):
+	def __init__(self,
+		img_size=64,
+		patch_size=8,
+		in_chans=1,
+		num_classes=2,
+		embed_dim=48,
+		window_size=2,
+		depths=[2, 2, 2, 2],
+		num_heads=[3, 6, 12, 24],
+	):
+		super().__init__()
+
+		self.swin_tr = SwinTransformer(
+			img_size=img_size,
+			patch_size=patch_size,
+			in_chans=in_chans,
+			num_classes=num_classes,
+			embed_dim=embed_dim,
+			window_size=window_size,
+			depths=depths,
+			num_heads=num_heads
+		)
+		self.encoders = nn.ModuleList()
+		for i in range(len(depths) + 2):
+			if i == 0:
+				in_size = in_chans
+				out_size = embed_dim
+			else:
+				in_size = embed_dim * 2 ** (i - 1)
+				out_size = embed_dim * 2 ** (i - 1)
+			encoder = UNetConvBlock(in_size=in_size, out_size=out_size)
+			self.encoders.append(encoder)
+
+		self.decoders = nn.ModuleList()
+		for i in range(len(depths) + 1):
+			if i == 0:
+				in_size=embed_dim
+				out_size=embed_dim
+				scale_factor = patch_size
+			else:
+				in_size=embed_dim * 2 ** (i)
+				out_size=embed_dim * 2 ** (i - 1)
+				scale_factor = 2
+			decoder = UNetUpBlock(in_size=in_size, out_size=out_size, scale_factor=scale_factor)
+			self.decoders.append(decoder)
+
+	def forward(self, x):
+		hidden_features = self.swin_tr(x)
+		enc = []
+		dec = []
+		for i in range(len(hidden_features) + 1):
+			if i == 0:
+				enc.append(self.encoders[0](x))
+			else:
+				enc.append(self.encoders[i](hidden_features[i - 1]))
+		
+		for i in range(len(hidden_features)):
+			if i == 0:
+				dec.append(self.decoders[-i - 1](enc[-1], hidden_features[-2]))
+			else:
+				dec.append(self.decoders[-i - 1](dec[i - 1], enc[-i - 2]))
+
+		return dec[-1]
