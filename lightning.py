4d57d39e2d3702a510910f547cee92aea45aceb8
Soma <somahansel@gmail.com>
2022-12-06 15:42:41 +0100

    import cleanup

--- lightning.py
+++ lightning.py
@@ -1,10 +1,5 @@
 import torch
 from torch import nn
-from torch.nn import functional as F
-from torch.utils.data import DataLoader
-from torch.utils.data import random_split
-from torchvision.datasets import MNIST
-from torchvision import transforms
 import pytorch_lightning as pl
 from pytorch_lightning.loggers import TensorBoardLogger
 from models.swin_transformer_block import SwinTransformer
--- models/swin_transformer_block.py
+++ models/swin_transformer_block.py
@@ -4,8 +4,6 @@ import torch.utils.checkpoint as checkpoint
 import torch.nn.functional as F
 from timm.models.layers import DropPath, to_3tuple, trunc_normal_
 
-import os, sys
-
 class Mlp(nn.Module):
     def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
         super().__init__()
