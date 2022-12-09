48ba11cbfd5cde9c974804a9b53fa751bfce06d9
Soma <somahansel@gmail.com>
2022-12-09 11:18:03 +0100

    data loader adjusted for whole DS

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,5 +1,6 @@
 from torch.utils.data import DataLoader, Dataset
 import torch
+from sklearn.model_selection import train_test_split
 
 import copy
 import glob
@@ -9,9 +10,14 @@ import numpy as np
 from scipy import ndimage
 import functools
 
-def getIdList():
-    raw_path_list = glob.glob('/home/somahansel/work/data/test/raw/*')
-    id_list = [os.path.split(path)[-1][:-7] for path in raw_path_list]
+def getTupleList():
+    raw_path_list_alt = glob.glob('../data/altesCT_Segmentierung/*')
+    raw_path_list_alt.sort()
+    id_list_alt = [(os.path.split(path)[-1][:-14], 'altesCT') for path in raw_path_list_alt]
+    raw_path_list_neu = glob.glob('../data/neuesCT_Segmentierung/*')
+    raw_path_list_neu.sort()
+    id_list_neu = [(os.path.split(path)[-1][:-14], 'neuesCT') for path in raw_path_list_neu]
+    id_list = id_list_alt + id_list_neu
     return id_list
 
 def getCenterCoords(img):
@@ -38,9 +44,9 @@ def getCenterCoords(img):
     return x, y, z
 
 @functools.lru_cache
-def getImages(id):
-    data = nib.load('./data/test/raw/{}.nii.gz'.format(id))
-    data_mask = nib.load('./data/test/mask/{}-labels.nii'.format(id))
+def getImages(id, scanner_version, image_size):
+    data = nib.load('../data/{}/{}.nii.gz'.format(scanner_version, id))
+    data_mask = nib.load('../data/{}_Segmentierung/{}-labels.nii.gz'.format(scanner_version, id))
     img = data.get_fdata()
     mask = data_mask.get_fdata()
 
@@ -58,27 +64,35 @@ def getImages(id):
         center_coord[2] - lengths[2]: center_coord[2] + lengths[2]
     ]
 
-    interpolation_zoom = 64 / np.asarray(img.shape)
+    interpolation_zoom = image_size / np.asarray(img.shape)
     img = ndimage.zoom(img, interpolation_zoom, order=1)
     mask = ndimage.zoom(mask, interpolation_zoom, order=1)
     return img, mask
 
 class SegmentationDataset(Dataset):
-    def __init__(self):
-        self.id_list = copy.copy(getIdList())
+    def __init__(self, tuple_list, image_size):
+        self.tuple_list = tuple_list
+        self.image_size = image_size
     
     def __len__(self):
-        return 1000
+        return len(self.tuple_list)
 
     def __getitem__(self, index):
-        id = self.id_list[0]
-        img, mask = getImages(id)
+        id = self.tuple_list[index][0]
+        scanner_version = self.tuple_list[index][1]
+        img, mask = getImages(id, scanner_version, image_size=self.image_size)
         img = torch.tensor(img)
         img = (img - torch.mean(img)) / torch.std(img)
         mask = torch.tensor(mask)
         # mask = (mask - torch.mean(mask)) / torch.std(mask)
         return img.unsqueeze(0).to(dtype=torch.float), mask.unsqueeze(0).to(dtype=torch.float)
 
-def getDataLoader(batch_size):
-    train_ds = SegmentationDataset()
-    return DataLoader(train_ds, batch_size=batch_size, drop_last=True)
+def getDataLoader(batch_size, image_size):
+    tuple_list = getTupleList()
+    tuple_list = tuple_list[:20]
+    train_list, val_list = train_test_split(tuple_list, test_size=0.1)
+    train_ds = SegmentationDataset(train_list, image_size)
+    val_ds = SegmentationDataset(val_list, image_size)
+    train_dl = DataLoader(train_ds, batch_size=batch_size, num_workers=48, drop_last=True)
+    val_dl = DataLoader(val_ds, batch_size=batch_size, num_workers=48, drop_last=True)
+    return train_dl, val_dl
