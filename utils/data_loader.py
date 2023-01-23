e3a88f325c13d465682ba621eb530ec4c03d8edc
Soma Hansel <hansel@john.iru.iam.loc>
2023-01-23 13:45:51 +0100

    dataloader: overfitting and augmentation

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -1,4 +1,5 @@
 from torch.utils.data import DataLoader, Dataset
+import torchvision.transforms.functional as F
 import torch
 from sklearn.model_selection import train_test_split
 
@@ -22,13 +23,25 @@ def getTupleListHDF5():
     return id_list_alt, id_list_neu
 
 @functools.lru_cache
-def getImagesHDF5(id, scanner, image_size):
+def loadImagesHDF5(id, scanner):
     file = h5py.File('../../data/segmentation/{}/{}.hdf5'.format(scanner[:3], id), 'r')
     img = np.array(file['img'][:])
     mask = np.array(file['mask'][:])
 
     center_coord = file.attrs['center_coords']
     center_coord = center_coord.astype(int)
+    return img, mask, center_coord
+
+def getImagesHDF5(id, scanner, image_size, aug):
+    # file = h5py.File('../../data/segmentation/{}/{}.hdf5'.format(scanner[:3], id), 'r')
+    # img = np.array(file['img'][:])
+    # mask = np.array(file['mask'][:])
+
+    # center_coord = file.attrs['center_coords']
+    # center_coord = center_coord.astype(int)
+
+    img, mask, center_coord = loadImagesHDF5(id, scanner)
+
     side_length = image_size // 2
     img = img[
         center_coord[0] - side_length: center_coord[0] + side_length,
@@ -41,12 +54,30 @@ def getImagesHDF5(id, scanner, image_size):
         center_coord[2] - side_length: center_coord[2] + side_length
     ]
 
+    if aug:
+        flip_axes = np.random.randint(low=0, high=2, size=3)
+        for i, i_flip in enumerate(flip_axes):
+            if i_flip > 0:
+                img = np.flip(img, i)
+                mask = np.flip(mask, i)
+
+        rotate_degrees = np.random.randint(low=0, high=4, size=3)
+        rotate_degrees = rotate_degrees * 90
+        for i, i_degree in enumerate(rotate_degrees):
+            img = ndimage.rotate(img, i_degree, axes=(i, (i + 1) % 3))
+            mask = ndimage.rotate(mask, i_degree, axes=(i, (i + 1) % 3))
+
+        scaling = np.random.rand()
+        scaling = (scaling * 0.2) + 0.9
+        img = scaling * img
+
     return img, mask
 
 class SegmentationDatasetHDF5(Dataset):
-    def __init__(self, tuple_list, image_size):
+    def __init__(self, tuple_list, image_size, aug=False):
         self.tuple_list = tuple_list
         self.image_size = image_size
+        self.aug=aug
     
     def __len__(self):
         return len(self.tuple_list)
@@ -54,12 +85,12 @@ class SegmentationDatasetHDF5(Dataset):
     def __getitem__(self, index):
         id = self.tuple_list[index][0]
         scanner = self.tuple_list[index][1]
-        img, mask = getImagesHDF5(id, scanner, image_size=self.image_size)
+        img, mask = getImagesHDF5(id, scanner, image_size=self.image_size, aug=self.aug)
         img = torch.tensor(img)
         mask = torch.tensor(mask)
         return img.unsqueeze(0).to(dtype=torch.float), mask.unsqueeze(0).to(dtype=torch.float)
 
-def getDataLoaderHDF5(batch_size, image_size, num_workers, data_ratio=1.0, persistent_workers=False):
+def getDataLoaderHDF5(batch_size, image_size, num_workers, data_ratio=1.0, persistent_workers=False, aug=False):
     tuple_list_alt, tuple_list_neu = getTupleListHDF5()
     train_list_alt, val_list_alt = train_test_split(tuple_list_alt, test_size=0.1, shuffle=False)
     train_list_neu, val_list_neu = train_test_split(tuple_list_neu, test_size=0.1, shuffle=False)
@@ -69,15 +100,18 @@ def getDataLoaderHDF5(batch_size, image_size, num_workers, data_ratio=1.0, persi
     train_list = train_list[:end_ndx_trn]
     if not data_ratio == 1.0:
         val_list = val_list[:batch_size]
-    # for overfitting
-    # train_list2 = []
-    # val_list2 = []
-    # for i in range(len(train_list)):
-    #     train_list2.append(tuple_list_alt[0])
-    # for i in range(len(val_list)):
-    #     val_list2.append(tuple_list_alt[0])
-    train_ds = SegmentationDatasetHDF5(train_list, image_size)
+    train_ds = SegmentationDatasetHDF5(train_list, image_size, aug=aug)
     val_ds = SegmentationDatasetHDF5(val_list, image_size)
+    train_dl = DataLoader(train_ds, batch_size=batch_size, num_workers=num_workers, drop_last=False, persistent_workers=persistent_workers)
+    val_dl = DataLoader(val_ds, batch_size=batch_size, num_workers=num_workers, drop_last=False, persistent_workers=persistent_workers, shuffle=False)
+    return train_dl, val_dl
+
+def getDataLoaderForOverfitting(batch_size, image_size, num_workers, persistent_workers=False, aug=False):
+    print('Using overfitting data loader')
+    tuple_list_alt, tuple_list_neu = getTupleListHDF5()
+    train_list = tuple_list_alt[:5] + tuple_list_neu[:5]
+    train_ds = SegmentationDatasetHDF5(train_list, image_size, aug=aug)
+    val_ds = SegmentationDatasetHDF5(train_list, image_size)
     train_dl = DataLoader(train_ds, batch_size=batch_size, num_workers=num_workers, drop_last=False, persistent_workers=persistent_workers)
     val_dl = DataLoader(val_ds, batch_size=batch_size, num_workers=num_workers, drop_last=False, persistent_workers=persistent_workers, shuffle=False)
     return train_dl, val_dl
