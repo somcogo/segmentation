c0ede511106b5a36512991e3ca9fabeb6ed4a307
Soma Hansel <hansel@john.iru.iam.loc>
2023-11-13 12:12:49 +0100

    grad accumulation, foreground pref chance

--- utils/ops.py
+++ utils/ops.py
@@ -2,13 +2,24 @@ import numpy as np
 import torch
 from scipy.ndimage import rotate
 
-def crop_section_and_aug(image, mask, section, image_size, pad=30, aug=False):
+def crop_section_and_aug(image, mask, section, image_size, pad=30, aug=False, foreground_pref_chance=0.):
     rng = np.random.default_rng()
+    pref_foreground = rng.uniform() < foreground_pref_chance
     H, W, D = image.shape
     h, w, d = image_size
-    x_0 = rng.integers(H - h)
-    y_0 = rng.integers(W - w)
-    z_0 = rng.integers(D - d)
+    if pref_foreground:
+        x_c, y_c, z_c = [arr.mean().astype(np.longlong) for arr in np.where(mask)]
+        x_low, x_high = max(0, x_c - h), min(H - h, x_c)
+        y_low, y_high = max(0, y_c - w), min(W - w, y_c)
+        z_low, z_high = max(0, z_c - d), min(D - d, z_c)
+    else:
+        x_low, x_high = 0, H - h
+        y_low, y_high = 0, W - w
+        z_low, z_high = 0, D - d
+
+    x_0 = rng.integers(x_low, x_high)
+    y_0 = rng.integers(y_low, y_high)
+    z_0 = rng.integers(z_low, z_high)
     x_1, y_1, z_1 = x_0 + h, y_0 + w, z_0 + d
     image = np.pad(image, pad, mode='edge')[x_0:x_1+2*pad, y_0:y_1+2*pad, z_0:z_1+2*pad]
     mask = np.pad(mask, pad, mode='constant', constant_values=0)[x_0:x_1+2*pad, y_0:y_1+2*pad, z_0:z_1+2*pad]
