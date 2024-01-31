3a697d4b0c559f12db8d1a9a9e6fae8fc7c4f5ad
Soma <somahansel@gmail.com>
2024-01-31 10:48:35 +0100

    postprocessing, dicom

--- utils/nii2dmc.py
+++ utils/nii2dmc.py
@@ -2,13 +2,14 @@ import SimpleITK as sitk
 import os
 import time
 import pydicom
-import nibabel
+import nibabel as nib
 import numpy as np
 from glob import glob
 
 
-def writeSlices(series_tag_values, new_img, i, out_dir):
+def writeSlices(series_tag_values, new_img, i, out_dir, zooms=(1, 1, 1)):
     image_slice = new_img[:,:,i]
+    # zooms = (zooms[0], zooms[1], 1)
 
     writer = sitk.ImageFileWriter()
     writer.KeepOriginalImageUIDOn()
@@ -21,18 +22,18 @@ def writeSlices(series_tag_values, new_img, i, out_dir):
     image_slice.SetMetaData("0008|0013", time.strftime("%H%M%S")) # Instance Creation Time
 
     # Setting the type to CT preserves the slice location.
-    image_slice.SetMetaData("0008|0060", "CT")  # set the type to CT so the thickness is carried over
+    # image_slice.SetMetaData("0008|0060", "CT")  # set the type to CT so the thickness is carried over
 
     # (0020, 0032) image position patient determines the 3D spacing between slices.
-    image_slice.SetMetaData("0020|0032", '\\'.join(map(str,new_img.TransformIndexToPhysicalPoint((0,0,i))))) # Image Position (Patient)
+    image_slice.SetMetaData("0020|0032", '\\'.join(map(str,new_img.TransformIndexToPhysicalPoint((0,0,int(i*zooms[2])))))) # Image Position (Patient)
     image_slice.SetMetaData("0020|0013", str(i)) # Instance Number
 
-    image_slice.SetMetaData("0020|1041", str(i)) # Slice location
-    print(new_img.TransformIndexToPhysicalPoint((0,0,i)))
+    image_slice.SetMetaData("0020|1041", str(i*zooms[2])) # Slice location
 
 
     # Write to the output directory and add the extension dcm, to force writing in DICOM format.
     writer.SetFileName(os.path.join(out_dir,'slice' + str(i).zfill(4) + '.dcm'))
+
     writer.Execute(image_slice)
 
 
@@ -46,20 +47,23 @@ def nifti2dicom_1file(in_dir, out_dir):
 
     os.makedirs(out_dir, exist_ok=True)
     img_id = in_dir[-14:-7]
-    print(img_id)
+    f = nib.load(in_dir)
+    zooms = f.header.get_zooms()
 
     new_img = sitk.ReadImage(in_dir)
     np_img = sitk.GetArrayFromImage(new_img)
     np_img = (np_img - np_img.min())/(np_img.max()-np_img.min()) * 255
+    if 'seg' in in_dir:
+        np_img = np_img[0]
     new_img = sitk.GetImageFromArray(np_img)
     new_img = sitk.Cast(new_img, sitk.sitkInt16)
+    print(np_img.shape)
     modification_time = time.strftime("%H%M%S")
     modification_date = time.strftime("%Y%m%d")
     uid = "1.2.826.0.1.3680043.2.1125."+modification_date+".1"+modification_time
 
     direction = new_img.GetDirection()
-    print((direction[0], direction[3], direction[6],# Image Orientation (Patient)
-                                                        direction[1],direction[4],direction[7]))
+    
     series_tag_values = [("0008|0031",modification_time), # Series Time
                     ("0008|0021",modification_date), # Series Date
                     ("0008|0008","DERIVED\\SECONDARY"), # Image Type
@@ -67,15 +71,14 @@ def nifti2dicom_1file(in_dir, out_dir):
                     ("0020|000d", uid), # Series Instance UID 
                     ("0008|0018", uid), # Series Instance UID 
                     ("0008|0030", modification_time), # Series Time
-                    ("0028|0030", str([1,1])), # Pixel spacing
-                    ("0020|0037", '\\'.join(map(str, (direction[0], direction[3], direction[6],# Image Orientation (Patient)
-                                                        direction[1],direction[4],direction[7])))),
+                    ("0028|0030", str(list(zooms[:2]))), # Pixel spacing
+                    ("0020|0037", '\\'.join(map(str, ([1.0, 0.0, 0.0, 0.0, 1.0, 0.0])))),# Image Orientation (Patient)
                     ("0008|103e", "Created-Pycad"), # Series Description
                     ("0010|0020", str(img_id)),
                     ]
 
     # Write slices to output directory
-    list(map(lambda i: writeSlices(series_tag_values, new_img, i, out_dir), range(new_img.GetDepth())))
+    list(map(lambda i: writeSlices(series_tag_values, new_img, i, out_dir, zooms), range(new_img.GetDepth())))
 
 def nifti2dicom_mfiles(nifti_dir, out_dir=''):
     """
@@ -124,7 +127,7 @@ def nifti2dicom_1file2(nifti_dir, out_dir):
     `out_dir`: the path to output
     """
 
-    nifti_file = nibabel.load(nifti_dir)
+    nifti_file = nib.load(nifti_dir)
     nifti_array = nifti_file.get_fdata()
     number_slices = nifti_array.shape[2]
 
